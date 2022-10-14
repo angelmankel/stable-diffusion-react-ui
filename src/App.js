d96959bc45b6d72c8b7641fff0aaaa754dab9c3e
angelmankel <angelmankel@gmail.com>
2022-10-14 12:55:26 -0700

    Starting over without Bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState, useLayoutEffect, useEffect, useRef } from 'react'
+import styled from 'styled-components';
+import { css } from 'styled-components';
 import logo from './img/logo.png';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/custom.css';
+import './css/Variables.css';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -14,6 +17,20 @@ import { faBrush, faCircleInfo, faCog, faImages, faPalette, faToolbox } from '@f
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image'
 
+import NavButton from './components/NavButton.jsx';
+
+// const Test = styled.div`
+//   background-color: blue;
+//   padding: 50px;
+// `
+
+const Test = styled(Container)`
+  background-color: blue;
+  padding: 50px;
+  ${props => props.red && css`
+    color: red;
+  `}
+`
 
 function App() {
 
@@ -22,6 +39,7 @@ function App() {
 
   const navbarRef = useRef(null)
   const mainRef = useRef(null)
+  const recentPanelRef = useRef(null)
 
   const getHeights = () => {
     setNavHeight(navbarRef.current.clientHeight)
@@ -35,81 +53,123 @@ function App() {
 
   }, [])
 
+  const RecentPanel = styled.div`
+    background-color: blue;
+    padding: 10px;
+    height: 160px;
+    display: flex;
+    flex-direction: row;
+  `
+
+  const RecallPanel = styled.div`
+    background-color: var(--secondary-panel-color);
+    border-radius: 10px;
+    box-shadow: 0 0 10px var(--shadow);
+    padding: 20px;
+    color: white;
+    height: 100%;
+  `
+
+  const ImageItem = styled.div`
+    background-color: white;
+    padding: 10px;
+    height: 100%;
+    width: 128px;
+    border-radius: 3px;
+    border: 1px solid #e0e0e0;
+    margin-right: 5px;
+  `
+
+  const MainContainer = styled(Container)`
+    height: 100vh;
+  `
+
+  let canvasHeight = 512
+  let canvasWidth = 512
+
   return (
+    <MainContainer fluid ref={mainRef}>
+      {/* NAV */}
+      {/* <Row className='custom-navbar' ref={navbarRef}>
+        <Col className="logo">
+          <img src={logo} className="app-logo" alt="LUMINARY" />
+          <h6>LUMINARY</h6>
+        </Col>
+        <Col lg={9} className='nav-buttons-container'>
+          <NavButton>Create</NavButton>
+          <button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faBrush} />{' '}CREATE</button>
+          <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faImages} />{' '}GALLERY</Button>
+          <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faPalette} />{' '}PROMPTS</Button>
+          <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faToolbox} />{' '}POST-PROCESSING</Button>
+          <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faCircleInfo} />{' '}ABOUT</Button>
+        </Col>
+        <Col className='nav-buttons-end'>
+          <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faCog} />{' '}</Button>
+        </Col>
+      </Row> */}
+      
+      {/* MAIN CONTENT */}
+      {/* <Row style={{"height" : contentHeight - navHeight}} className='main-content'>
+        <Col>
+          <Row>
+            <Col>
+              <Row className='bg-info'>
+                {/* RECALL PANEL */}
+                <Col>
+                  <RecallPanel>
+                    panel test
+                  </RecallPanel>
+                </Col>
+                {/* PROMPT & CANVAS */}
+                <Col>
+                  {/* PROMPT INPUT */}
+                  <Row>
+                    <Col>
+                      <FloatingLabel controlId="floatingTextarea2" label="Prompt">
+                        <Form.Control
+                          as="textarea"
+                          placeholder="Enter a prompt"
+                          style={{ height: '80px' }}
+                        />
+                      </FloatingLabel>
+                    </Col>
+                  </Row>
+                  {/* CANVAS */}
+                  <Row>
+                    <Col>
+                    <Image
+                      height={canvasHeight}
+                      width={canvasWidth}
+                      src="https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
+                      rounded
+                    />
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <RecentPanel>
+                <ImageItem />
+                <ImageItem />
+                <ImageItem />
+                <ImageItem />
+                <ImageItem /> 
+                <ImageItem /> 
+              </RecentPanel>
+            </Col>
+          </Row>
+        </Col>
+        <Col lg={3}>
+          <div className='panel'>
+            panel test
+          </div>
+        </Col>
+      </Row> */}
 
-    <div className="main" ref={mainRef}>
-      <Container fluid className='fill'>
-        {/* NAV */}
-        <Row className='custom-navbar' ref={navbarRef}>
-          <Col className="logo">
-            <img src={logo} className="app-logo" alt="LUMINARY" />
-            <h6>LUMINARY</h6>
-          </Col>
-          <Col lg={9} className='nav-buttons-container'>
-            <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faBrush} />{' '}CREATE</Button>
-            <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faImages} />{' '}GALLERY</Button>
-            <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faPalette} />{' '}PROMPTS</Button>
-            <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faToolbox} />{' '}POST-PROCESSING</Button>
-            <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faCircleInfo} />{' '}ABOUT</Button>
-          </Col>
-          <Col className='nav-buttons-end'>
-            <Button className='custom-nav-btn'><FontAwesomeIcon className="icon" icon={faCog} />{' '}</Button>
-          </Col>
-        </Row>
-        
-        {/* MAIN CONTENT */}
-        <Row style={{"height" : contentHeight - navHeight}} className='main-content'>
-          <Col>
-            <Row>
-              <Col>
-                <Row>
-                  <Col>
-                    <div className='panel'>
-                      panel test
-                    </div>
-                  </Col>
-                  <Col>
-                    {/* PROMPT INPUT */}
-                    <Row>
-                      <Col>
-                        <FloatingLabel controlId="floatingTextarea2" label="Prompt">
-                          <Form.Control
-                            as="textarea"
-                            placeholder="Enter a prompt"
-                            style={{ height: '80px' }}
-                          />
-                        </FloatingLabel>
-                      </Col>
-                    </Row>
-                    {/* CANVAS */}
-                    <Row>
-                      <Col>
-                      <Image
-                        src="https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
-                        rounded
-                      />
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-                <h1 className='bg-info'>MAIN CONTENT</h1>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h1 className='bg-info'>MAIN CONTENT</h1>
-              </Col>
-            </Row>
-          </Col>
-          <Col lg={3}>
-            <div className='panel'>
-              panel test
-            </div>
-          </Col>
-        </Row>
-
-      </Container>
-    </div>
+    </MainContainer>
 
   )
 }
--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,60 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBrush, faCircleInfo, faCog, faImages, faPalette, faToolbox } from '@fortawesome/free-solid-svg-icons'
-import logo from './logo.png';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import styled from 'styled-components';
 
-let navBtnStyle = {"outline" : "0px solid gray"}
-let imgStyle = {"width" : 40, "marginRight" : 7}
+const Button = styled.div`
+  cursor: pointer;
+  font-size: 1pc;
+  text-shadow: 4px 3px 9px rgba(0,0,0,1);
+  padding-left: 30px;
+  padding-right: 30px;
+  height: 100%;
+  border: none;
+  border-radius: 0px !important;
+  background-color: var(--transparent);
+  
+  &:hover {
+    background-color: var(--btn-hover-primary) !important;
+    box-shadow: 0px 0px 10px var(--shadow);
+  }
+`
 
 function NavButton(props) {
   console.log(props)
   return (
-    <Navbar bg="dark" variant="dark" expand="md">
-      <Container fluid>
-        <Navbar.Brand href="#home">
-          <img src={logo} style={imgStyle} alt="Logo" />
-          App Name
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link style={navBtnStyle}>
-              
-              Create
-            </Nav.Link>
-            <Nav.Link style={navBtnStyle}>
-              
-              <FontAwesomeIcon icon="fa-solid fa-gallery-thumbnails" />
-              Gallery
-            </Nav.Link>
-            <Nav.Link style={navBtnStyle}>
-              
-              Prompts
-            </Nav.Link>
-            <Nav.Link style={navBtnStyle}>
-              
-              Post-Processing
-            </Nav.Link>
-            <Nav.Link style={navBtnStyle}>
-              
-              About
-            </Nav.Link>
-            <Nav.Link style={navBtnStyle}>
-              
-              Settings
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Button>{props.children}</Button>
   )
 }
 
